5aa10edd725e07fdd65699e8fa685fe0631064ec
veeresh <veereshmh407@gmail.com>
2025-12-19 14:32:17 +0530

    First Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
 
-function App() {
-  
-
-  return (
-    <>
-      <div>
-        <h1>New Task001</h1>
-        </div>
-    </>
-  )
+function Home() {
+  return <h2>Home Page</h2>;
 }
 
-export default App
+function About() {
+  return <h2>About Page</h2>;
+}
+
+export default function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>React App Deployed Successfully 🚀</h1>
+
+      <nav style={{ marginBottom: "20px" }}>
+        <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
